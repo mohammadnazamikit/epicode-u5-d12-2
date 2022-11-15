56c897c6ebcfb9d15d8d2f339e1dbdfbcd7e4947
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-11-15 17:18:05 +0100

    update 1

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -1,57 +1,113 @@
 // By default jest does not work with the new import syntax
 // We should add NODE_OPTIONS=--experimental-vm-modules to the package.json (test script) to enable that
 // ON WINDOWS YOU HAVE TO USE CROSS-ENV PACKAGE TO BE ABLE TO PASS ENV VARS TO COMMAND LINE SCRIPTS!!
-import supertest from "supertest"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import server from "../src/server.js"
-import ProductsModel from "../src/products/model.js"
+import supertest from "supertest";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import server from "../src/server.js";
+import ProductsModel from "../src/products/model.js";
 
-dotenv.config() // This command forces .env variables to be loaded into precess.env. This is the way to go when you can't use -r dotenv/config
+dotenv.config(); // This command forces .env variables to be loaded into precess.env. This is the way to go when you can't use -r dotenv/config
 
 // supertest is capable of executing server.listen if we pass the Express server to it, it will give us back a client to be used to run http requests on that server
-const client = supertest(server)
+const client = supertest(server);
 
 const newProduct = {
-  name: "test",
-  description: "bal bla bla",
-  price: 29,
-}
+  name: "Nazami",
+  description: "this is Nazami",
+  price: 10,
+};
 
 const notValidProduct = {
   description: "bal bla bla",
   price: 29,
-}
+};
 
-beforeAll(async () => {
-  // beforeAll hook could be used to connect to Mongo and doing some initial setup before running tests ( like inserting mock data into collections)
-  await mongoose.connect(process.env.MONGO_TEST_CONNECTION)
-  const product = new ProductsModel(newProduct)
-  await product.save()
-})
+const updateProduct = {
+  name: "ahmad",
+};
 
-afterAll(async () => {
-  // afterAll hook could be used to close the connection to Mongo gently and clean up db/collections
-  await ProductsModel.deleteMany()
-  await mongoose.connection.close()
-})
-
-describe("Test Products APIs", () => {
+/* describe("Test Products APIs", () => {
   it("Should check that Mongo connection string is not undefined", () => {
-    expect(process.env.MONGO_TEST_CONNECTION).toBeDefined()
-  })
+    expect(process.env.MONGO_TEST_CONNECTION).toBeDefined();
+  });
 
   it("Should test that GET /products returns a success status and a body", async () => {
-    const response = await client.get("/products").expect(200)
-    console.log(response.body)
-  })
+    const response = await client.get("/products").expect(200);
+    console.log(response.body);
+  });
 
   it("Should test that POST /products returns a valid _id and 201", async () => {
-    const response = await client.post("/products").send(newProduct).expect(201)
-    expect(response.body._id).toBeDefined()
-  })
+    const response = await client
+      .post("/products")
+      .send(newProduct)
+      .expect(201);
+    expect(response.body._id).toBeDefined();
+  });
 
   it("Should test that POST /products with a not valid product returns 400", async () => {
-    await client.post("/products").send(notValidProduct).expect(400)
-  })
-})
+    await client.post("/products").send(notValidProduct).expect(400);
+  });
+}); */
+
+describe("test for homework", () => {
+  const notValidId = "123456123456123456123456";
+  let _id = null;
+  let data = null;
+  beforeAll(async () => {
+    // beforeAll hook could be used to connect to Mongo and doing some initial setup before running tests ( like inserting mock data into collections)
+    await mongoose.connect(process.env.MONGO_TEST_CONNECTION);
+    const product = new ProductsModel(newProduct);
+    _id = product._id;
+    await product.save();
+  });
+
+  afterAll(async () => {
+    // afterAll hook could be used to close the connection to Mongo gently and clean up db/collections
+    await ProductsModel.deleteMany();
+    await mongoose.connection.close();
+  });
+
+  it("get all products with 200 statue code", async () => {
+    const data = await client.get("/products").expect(200);
+    console.log(data.body);
+  });
+
+  it("create new product with 201 statue code", async () => {
+    const data = await client.post("/products").send(newProduct).expect(201);
+    console.log(data);
+    _id = data.body._id;
+  });
+
+  it("create new product with not valid entities", async () => {
+    await client.post("/products").send(notValidProduct).expect(400);
+  });
+
+  it("retrieve not valid product id ", async () => {
+    await client.get("/products/:id").send(notValidId).expect(404);
+  });
+
+  it("retrieve valid product id", async () => {
+    console.log(_id, "-----------------------id");
+    await client.get(`/products/${_id}`).send().expect(200);
+  });
+
+  it("updating with id", async () => {
+    const updatedProduct = await client
+      .put(`/products/${_id}`)
+      .send(updateProduct)
+      .expect(200);
+  });
+
+  it("update not successfull", async () => {
+    await client.put(`/products/${notValidId}`).send(updateProduct).expect(404);
+  });
+
+  it("deleting by Id", async () => {
+    await client.delete(`/products/${_id}`).send().expect(204);
+  });
+
+  it("deleting not valid Id product", async () => {
+    await client.delete(`/products/${notValidId}`).send().expect(404);
+  });
+});
--- src/products/index.js
+++ src/products/index.js
@@ -1,26 +1,64 @@
-import express from "express"
-import createHttpError from "http-errors"
-import ProductsModel from "./model.js"
+import express from "express";
+import createHttpError from "http-errors";
+import ProductsModel from "./model.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter.post("/", async (req, res, next) => {
   try {
-    const newProduct = new ProductsModel(req.body)
-    const { _id } = await newProduct.save()
-    res.status(201).send({ _id })
+    const newProduct = new ProductsModel(req.body);
+    const { _id } = await newProduct.save();
+    res.status(201).send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const products = await ProductsModel.find({})
-    res.send(products)
+    const products = await ProductsModel.find({});
+    res.send(products);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default productsRouter
+productsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    next(createHttpError(404, `the product you searching for, not found`));
+  }
+});
+
+productsRouter.put("/:Id", async (req, res, next) => {
+  try {
+    const updatedProduct = await ProductsModel.findByIdAndUpdate(
+      req.params.Id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedProduct) {
+      res.send(updatedProduct);
+    } else {
+      next(createHttpError(404, `user with id ${req.params.Id} didnt found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+productsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndDelete(req.params.id);
+    if (product) {
+      res.status(204).send();
+    } else {
+      next(createHttpError(404, "the product you searching for not found"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+export default productsRouter;
